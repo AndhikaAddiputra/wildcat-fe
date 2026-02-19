d945392e0fb0373887415753fa73cdb4c040c857
ano <fatihsefer502@gmail.com>
2026-02-19 21:31:23 +0700

    style: configure label style

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -2,22 +2,30 @@ import { type HTMLAttributes } from "react";
 import { cn } from "@/lib/utils";
 
 export interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
-  variant?: "pending" | "verified" | "complete" | "end";
+  variant?: "default" | "pending-s" | "verified-s" | "complete-s" | "pending" | "verified" | "complete" | "end";
 }
 
-function Badge({ className, variant = "pending", ...props }: BadgeProps) {
+function Badge({ className, variant = "default", ...props }: BadgeProps) {
   return (
     <span
       className={cn(
-        "items-center border-3 rounded-[20px] h-[40px] w-[160px] px-2.5 py-0.5 text-xs font-medium transition-colors bg-darkblue flex justify-center",
+        "items-center border-3 rounded-[20px] h-[40px] w-[147px] px-2.5 py-0.5 text-[16px] font-bold transition-colors bg-darkblue flex justify-center before:content-[''] before:mr-2 before:w-2 before:h-2 before:rounded-full before:bg-current",
         {
+          "bg-zinc-100 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-300":
+            variant === "default",
           "text-yellow border-yellow":
-            variant === "pending",
+            variant === "pending-s",
           "text-orange border-orange":
-            variant === "verified",
+            variant === "verified-s",
           "text-green border-green":
+            variant === "complete-s",
+          "text-yellow border-yellow w-[200px]":
+            variant === "pending",
+          "text-orange border-orange w-[200px]":
+            variant === "verified",
+          "text-green border-green w-[200px]":
             variant === "complete",
-          "text-red border-red":
+          "text-red border-red w-[200px]":
             variant === "end",
         },
         className
